1fae86460d791164c979c8a674508ca7b5c45436
masterCha <32441322+masterCha@users.noreply.github.com>
2017-10-01 23:47:36 +0100

    Update UserDAO.java

--- src/main/java/ua/application/dao/interfaces/UserDAO.java
+++ src/main/java/ua/application/dao/interfaces/UserDAO.java
@@ -5,17 +5,17 @@ import java.util.List;
 import ua.application.users.Users;
 
 public interface UserDAO {
-
+	// Find user by login
 	Users find(String login);
-
+	// Insert new user to database
 	void insert(Users user);
-
+	// Delete user from database
 	void delete(int id);
-
+	// Rewrite user data in database
 	void rewrite(int id, Users updatedUser);
-
+	// Get list of users by name
 	List<Users> getUsersByName(String name);
-
+	// Get list of all users
 	List<Users> getAllUsers();
 
 }
